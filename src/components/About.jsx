cb0377edc4ff42afa9d359876a0d039f8c8fd27e
bhargavch7 <bhargav.dev01@gmail.com>
2024-10-10 19:04:40 +0530

    Initial deploy to gh-pages

---
+++ src/components/About.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../App.css';
+import { services } from '../constants';
+import ButtonLink from './ButtonLink';
+
+const ServiceCard = ({ service }) => (
+  <div className='sm:w-[250px] w-full'>
+    <div
+      className='w-full green-pink-gradient p-[1px] rounded-[20px]'>
+      <div
+        className='rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        style={{ background: '#151030' }}>
+        <img
+          src={service.icon}
+          alt='some_icon'
+          className='w-16 h-16 object-contain'
+        />
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {service.title}
+        </h3>
+      </div>
+    </div>
+  </div>
+);
+
+const About = () => {
+  return (
+    <div className='bg-black p-4 h-full w-full text-white sm:flex sm:justify-around about' id='about'>
+      <div className='flex flex-col justify-around'>
+        <div className='sm:px-16 px-2'>
+        <h2 className='text-4xl sm:text-5xl font-extrabold mt-2'>Introduction</h2>
+            <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>👨‍💻 Hi, I'm Bhargav Chitteti, a Backend Developer with a passion for building scalable web applications and robust backend systems. As a seasoned <a className='text-green-300 hover:text-green-500 duration-300' href='https://www.linkedin.com/in/nithin-manda-728019214/' target='_blank'>Backend Developer</a>, I specialize in Python and Django stacks🚀.
+              <br />
+              ✍️ In my free time, I explore new technical stacks, creating <a className='text-blue-300 hover:text-blue-500 duration-300' href='https://github.com/bhargavch' target='_blank'>GitHub projects</a> and sharing resources for other tech enthusiasts, making learning easier.
+              <br />
+              🌍 When I’m not coding, I enjoy traveling, cooking, and watching cricket, which help me balance work with personal passions.
+            </p>
+
+          <ButtonLink
+            url='https://drive.google.com/drive/u/0/folders/1N_cRa01DbAr25eBvJfM-wlky_W-Cx0Yh'
+            text='View Resume Neeedd to update →'
+            padding={`p-3`}
+          />
+        </div>
+      </div>
+      <img src="" alt="Bhargav chitteti" className='sm:w-1/3 mt-4 sm:mt-0' />
+    </div>
+  )
+}
+
+export default About;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FaGithub, FaTwitter } from 'react-icons/fa';
+import { AiFillMediumCircle, AiFillLinkedin } from 'react-icons/ai';
+import { FiMail } from 'react-icons/fi';
+import { BsStackOverflow } from 'react-icons/bs';
+import { BiLink } from 'react-icons/bi';
+
+
+const Footer = () => {
+	const year = new Date().getFullYear();
+
+	return (
+		<div className='bg-slate-900 p-4' style={{ cursor: 'default' }}>
+			<div className='flex justify-between flex-wrap gap-4'>
+				<p className='text-white text-center w-full sm:w-auto font-light'>© {year} csgeek. All rights reserved.</p>
+				<div className='text-white flex justify-around sm:w-[250px] w-full'>
+					<a href="mailto:bhargav.dev01@gmail.com" className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<FiMail className='text-xl' />
+					</a>
+					<a href="https://github.com/csgeeek" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }} >
+						<FaGithub className='text-xl' />
+					</a>
+					<a href="https://www.linkedin.com/in/balaji-yaswanth/" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<AiFillLinkedin className='text-xl' />
+					</a>
+					{/* <a href="https://bio.link/csgeek" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<BiLink className='text-xl' />
+					</a> */}
+					{/* <a href="https://twitter.com/csgeeek" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<FaTwitter className='text-xl' />
+					</a> */}
+					{/* <a href="https://stackoverflow.com/users/12291279/csgeek" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<BsStackOverflow className='text-xl' />
+					</a> */}
+					{/* <a href="https://medium.com/@balajiyaswanth.v" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+						<AiFillMediumCircle className='text-xl' />
+					</a> */}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
